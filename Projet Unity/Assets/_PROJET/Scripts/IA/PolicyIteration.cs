bea21ba1ac7fa8055f93194b9b6ed6e1c667542e
Nikow <nicolas@ruche.me>
2022-02-17 16:37:48 +0100

    feat: begin policy iteration

---
+++ Projet Unity/Assets/_PROJET/Scripts/IA/PolicyIteration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PGSauce.Games.IaEsgi.Ia
+{
+    public abstract class PolicyIteration<TAgent, TState> : QAlgorithm<TAgent, TState> where TAgent : QAgentBase where TState : QState
+    {
+        private Dictionary<TState, float> _v;
+
+        protected sealed override void CustomBeforeExecute()
+        {
+            _v = new Dictionary<TState, float>();
+            foreach (var state in States)
+            {
+                _v.Add(state, 0f);
+            }
+        }
+    }
+}
